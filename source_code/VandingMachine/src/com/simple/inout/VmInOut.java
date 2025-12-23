e475575a6afa42dbcb1e965f600418c34ade2c22
Jong Cheon Park <simpleego@gmail.com>
2025-12-23 09:57:04 +0900

    Update VmInOut.java

--- source_code/VandingMachine/src/com/simple/inout/VmInOut.java
+++ source_code/VandingMachine/src/com/simple/inout/VmInOut.java
@@ -9,12 +9,13 @@ public class VmInOut {
 		// 밀크커피(300원)
 		
 		// 프로그램에 관련된 입출력 변수 준비
-		int inCoin;
+		int inCoin = 0;
 		String milkCoffee="밀크커피";
 		int milkPrice = 300;
 		boolean milkOk=false;
 		int menu;
 		int vmCoin = 0;
+		String coinRepeat="n";
 		
 		Scanner kbd = new Scanner(System.in);
 				
@@ -26,11 +27,17 @@ public class VmInOut {
 				milkOk ? "○" : "X");
 			
 		// 동전 입력
-		System.out.println("동전 입력: ");
-		inCoin = kbd.nextInt();
-		
-		// 입력한 동전 출력
-		System.out.println(" 잔액 : "+inCoin);	
+		do {
+			System.out.println("동전 입력: ");
+			inCoin += kbd.nextInt();
+			kbd.nextLine();
+			
+			// 입력한 동전 출력
+			System.out.println(" 잔액 : "+inCoin);
+			System.out.println("동전입력 계속하나요? y/n");
+			coinRepeat = kbd.nextLine();
+			
+		}while(coinRepeat.equals("y") );
 		
 		// 밀크 커피 판매 가능 상태로 변환 처리
 		// 입력한 동전이 300원 이상이면 판매가능 상태 "O"로 변환
@@ -38,11 +45,7 @@ public class VmInOut {
 			milkOk = true;
 		}
 		
-		System.out.printf("%s:(%d원) %s\n",
-				milkCoffee,
-				milkPrice,
-				milkOk ? "○" : "X");	
-		
+			
 		// 밀크커피 판매 처리
 		// 커피 선택하기
 		// 판매에 따른 처리
@@ -50,7 +53,7 @@ public class VmInOut {
 				milkCoffee,
 				milkPrice,
 				milkOk ? "○" : "X");
-		System.out.print("메뉴 선택 : 밀크커피 : 1 ");
+		System.out.println("메뉴 선택 : 밀크커피:1 ");
 		menu = kbd.nextInt();
 		
 		switch(menu) {
@@ -59,16 +62,21 @@ public class VmInOut {
 			vmCoin += milkPrice;
 			inCoin -= milkPrice;
 			System.out.println(milkCoffee+" 나왔습니다.");			
-		}		
+		}	
 		
-		System.out.printf("%s:(%d원) %s %d \n",
+		// 밀크커피 상태 체크
+		if(inCoin >= milkPrice) {
+			milkOk = true;
+		}else {
+			milkOk = false;
+		}	
+		
+		
+		System.out.printf("%s:(%d원) %s \n",
 				milkCoffee,
 				milkPrice,
-				milkOk ? "○" : "X",
-				inCoin);
-		
-		
-		
+				milkOk ? "○" : "X");
+		System.out.println("잔액 : "+inCoin);
 		
 	}
 
