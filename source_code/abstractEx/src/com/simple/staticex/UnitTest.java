ff1ea3f68e5cf5ed47f22f7087726a9eb1ed3682
Jong Cheon Park <simpleego@gmail.com>
2025-12-29 13:16:22 +0900

    Add files via upload

---
+++ source_code/abstractEx/src/com/simple/staticex/UnitTest.java
@@ -0,0 +1,18 @@
+package com.simple.staticex;
+class UnitConverter{
+    // cm -> m
+    public static double cmToMeter(double cm) {
+        return cm/100.0;
+    }
+}
+
+
+public class UnitTest {
+    public static void main(String[] args) {
+
+        UnitConverter.cmToMeter(214.1);
+
+        Math.random();
+
+    }
+}
