ce63bdc224f2fd0b73cba8bf7f722d1ef4e24700
Jong Cheon Park <simpleego@gmail.com>
2025-12-31 07:53:07 +0900

    Add files via upload

---
+++ source_code/ExceptionEx/src/com/simple/ex/ExceptionEx01.java
@@ -0,0 +1,23 @@
+package com.simple.ex;
+
+import java.io.IOException;
+
+public class ExceptionEx01 {
+    public static void main(String[] args) {
+//        int a = 0;
+//        int b = 5/a;
+
+        try {
+            int a=0;
+            int b = 5/a;
+        }catch(IndexOutOfBoundsException e){
+            // e.printStackTrace();
+            System.out.println("배열 범위 초과했습니다.");
+        }catch(NullPointerException e){
+            System.out.println("객체가 없습니다.");
+        }catch (RuntimeException e){
+            System.out.println(" 실행시 예외가 발생했습니다.");
+        }
+        System.out.println(" 정상적으로 다음 문장 수행합니다.");
+    }
+}
